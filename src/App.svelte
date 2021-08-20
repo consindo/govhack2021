602eebcd1ce02bdb66e32187cf5fc6904b57d783
Jono Cooper <jono@jonocooper.com>
2021-08-20 22:44:37 +1200

    show mins and distance to enduser nicely

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,32 @@
 <script>
   import { plan, roadPlan, processPlan, processRoadPlan } from './clients/aucklandtransport.js'
 
-  const LOADING_STR = 'Loading from Auckland Transport...'
-  $: ptPlan = LOADING_STR
-  $: cyclePlan = LOADING_STR
-  $: walkPlan = LOADING_STR
-  $: drivePlan = LOADING_STR
+  $: itineraries = []
 
-  plan().then(processPlan).then(data => {
-    ptPlan = JSON.stringify(data, '', 2)
-  })
-  roadPlan('cycle').then(processRoadPlan('cycle')).then(data => {
-    cyclePlan = JSON.stringify(data, '', 2)
-  })
-  roadPlan('walk').then(processRoadPlan('walk')).then(data => {
-    walkPlan = JSON.stringify(data, '', 2)
-  })
-  roadPlan('drive').then(processRoadPlan('drive')).then(data => {
-    drivePlan = JSON.stringify(data, '', 2)
-  })
-
+  const cb = data => {
+    console.log(data)
+    const newItineraries = itineraries
+    data.processed.forEach(i => newItineraries.push(i))
+    itineraries = newItineraries
+  }
+  plan().then(processPlan).then(cb)
+  roadPlan('cycle').then(processRoadPlan('cycle')).then(cb)
+  roadPlan('walk').then(processRoadPlan('walk')).then(cb)
+  roadPlan('drive').then(processRoadPlan('drive')).then(cb)
 
 </script>
 
 <main>
   <h1>govhack2021</h1>
-  <p>directions from britomart to panmure</p>
-  <h2>directions via public transport</h2>
-  <pre>{ptPlan}</pre>
-  <h2>directions via cycling</h2>
-  <pre>{cyclePlan}</pre>
-  <h2>directions via walking</h2>
-  <pre>{walkPlan}</pre>
-  <h2>directions via driving</h2>
-  <pre>{drivePlan}</pre>
+  <p>directions from Britomart to Panmure</p>
+  {#if itineraries.length === 0}
+    Loading from Auckland Transport...
+  {/if}
+  {#each itineraries as itinerary}
+    <h2>{itinerary.total.description}</h2>
+    <p><strong>Minutes:</strong> {itinerary.total.timeMinutes}</p>
+    <p><strong>Distance:</strong> {Math.round(itinerary.total.distanceKilometers * 100) / 100}km</p>
+  {/each}
 
 
 </main>
